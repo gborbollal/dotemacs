daa2c10ebf83ba8fa9124763c871eddd0986b121
Gerardo Borbolla <gerardo.borbolla@valimail.com>
2023-04-28 12:24:08 -0700

    Translate Alt/ESC using keymaps

--- dotemacs_mac.el
+++ dotemacs_mac.el
@@ -1,14 +1,22 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-save-default nil)
- '(delete-old-versions t)
+ '(global-auto-revert-mode t)
+ '(help-window-select t)
  '(make-backup-files nil)
+ '(neo-show-hidden-files t)
  '(package-selected-packages
-   '(winring zygospore burly use-package frog-jump-buffer web-mode zel helm-dired-recent-dirs duplicate-thing move-text robe helm-ag inf-ruby company base16-theme fzf magit ##))
- '(savehist-mode t))
+	 '(backward-forward helm-fuzzy-find helm-ag helm-projectile projectile use-package go-mode json-mode neotree yaml-mode helm-lsp helm flycheck lsp-ui dap-mode company))
+ '(savehist-mode t)
+ '(tab-width 2)
+ '(visible-bell t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,56 +24,42 @@
  ;; If there is more than one, they won't work right.
  )
 
+(require 'helm)
+(helm-mode 1)
+(projectile-global-mode)
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
 
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+(add-hook 'after-init-hook 'global-company-mode)
+(load "~/.emacs.d/editor.el")
+(load-theme 'manoj-dark)
 
-(require 'zel)
-(require 'bind-key)
-(load-theme 'base16-phd t)
-(add-hook 'prog-mode-hook 'company-mode)
+(global-set-key (kbd "C-d") 'duplicate-current-line-or-region)
+(global-set-key (kbd "C-q") 'kill-current-buffer)
+(global-set-key (kbd "M-1") 'neotree-toggle)
+(global-set-key (kbd "M-f") 'scroll-down)
+(global-set-key (kbd "M-v") 'scroll-up)
+(global-set-key (kbd "M-q") 'switch-to-buffer)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-o") 'helm-projectile)
+(global-set-key (kbd "C-b") 'godef-jump)
 
-(global-set-key (kbd "s-}") (lambda () (interactive) (other-window 1)))
-(global-set-key (kbd "s-{") (lambda () (interactive) (other-window -1)))
-(global-set-key (kbd "s-S") 'helm-do-ag-project-root)
-(global-set-key (kbd "s-o") 'fzf-git)
-(global-set-key (kbd "s-w") (lambda () (interactive) (kill-buffer (current-buffer))))
-(global-set-key (kbd "s-p") 'find-file)
-(global-set-key (kbd "s-9") 'magit-status)
-(bind-key* "<M-tab>" 'helm-buffers-list)
-(global-set-key (kbd "M-<up>") 'move-text-up)
-(global-set-key (kbd "M-<down>") 'move-text-down)
-(global-set-key (kbd "s-d") 'duplicate-thing)
-(global-set-key (kbd "s-e") 'zel-find-file-frecent)
-(global-set-key (kbd "s-s") 'save-buffer)
-(global-set-key (kbd "s-e") 'eval-last-sexp)
+;; fix iTerm2 bugs. Use the quoted-insert function (C-q) to get the character sequence
+;; Add the sequences to the standard emacs keymap for escape sequences
+(define-key esc-map [?\[ ?1]
+	(let ((arrow-map (make-sparse-keymap)))
+		(define-key arrow-map [?\; ?9 ?D] 'backward-word)
+		(define-key arrow-map [?\; ?9 ?C] 'forward-word)
+		(define-key arrow-map [?\; ?1 ?0 ?C] 'other-window)
+		(define-key arrow-map [?\; ?1 ?0 ?D] #'(lambda () (interactive) (other-window -1)))
+		arrow-map))
 
-(global-set-key (kbd "<s-up>") 'buf-move-up)
-(global-set-key (kbd "<s-left>") 'buf-move-left)
-(global-set-key (kbd "<s-right>") 'buf-move-right)
-(global-set-key (kbd "<s-down>") 'buf-move-down)
+(add-hook 'go-mode-hook 'lsp-deferred)
+(dolist (hook '(go-mode-hook elisp-mode-hook yaml-mode-hook))
+	(add-hook hook 'backward-forward-mode)
+	(add-hook hook 'display-line-numbers-mode))
 
-(global-set-key (kbd "s-1") 'zygospore-toggle-delete-other-windows)
-(require 'winring)
-(setq winring-keymap-prefix (kbd "s-2"))
-(winring-initialize)
+(require 'dap-dlv-go)
 
-(global-set-key (kbd "s-3") 'burly-open-bookmark)
-
-(add-to-list 'load-path "~/.emacs.d/vendors/emacs-powerline")
-(add-to-list 'load-path "~/.emacs.d/vendors/buffer-move")
-(require 'powerline)
-(require 'buffer-move)
-
-(set-face-attribute 'mode-line nil
-                    :foreground "Black"
-                    :background "DarkOrange"
-                    :box nil)
-
-(add-hook 'ruby-mode-hook 'robe-mode)
-(eval-after-load 'company
-  '(push 'company-robe company-backends))
-
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(setq gofmt-command "goimports")
+(add-hook 'before-save-hook 'gofmt-before-save)
